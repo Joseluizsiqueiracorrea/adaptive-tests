d5e13326fc8670a3a3eef5752a8bc35fca4da9ad
anon57396 <jasonkempf@gmail.com>
2025-09-21 11:29:55 -0500

    refactor: remove deprecated scripts and clean up package.json

--- scripts/build-plugins.js
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env node
-
-console.log('ℹ️  build:plugins: no build step required (preview plugins ship as plain JS).');
-console.log('    If you add bundling in the future, update scripts/build-plugins.js accordingly.');
